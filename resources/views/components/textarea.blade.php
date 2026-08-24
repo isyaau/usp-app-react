29c14e29d62f441c28592251abe848d309361c02
isyaau <isak.akhsanil@gmail.com>
2026-08-24 12:24:36 +0700

    update

--- resources/views/components/textarea.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="mb-3">
-    @if($label)
-    <label class="form-label">{{ $label }}</label>
-    @endif
-    <textarea {{ $attributes->merge(['class' => 'form-control']) }}>{{ $slot ?? '' }}</textarea>
-    @error($attributes->get('wire:model'))
-    <div class="text-danger small">{{ $message }}</div>
-    @enderror
-</div>
